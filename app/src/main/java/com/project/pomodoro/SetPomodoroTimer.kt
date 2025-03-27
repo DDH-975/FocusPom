7a2cde35ab3146759119168ee6917e97f0095465
junho <junho5508@naver.com>
2025-03-28 00:13:44 +0900

    진동 알림 기능 추가, 공부시간 계산 로직 추가

--- app/src/main/java/com/project/pomodoro/SetPomodoroTimer.kt
+++ app/src/main/java/com/project/pomodoro/SetPomodoroTimer.kt
@@ -1,6 +1,9 @@
 package com.project.pomodoro
 
+import android.os.Build
 import android.os.CountDownTimer
+import android.os.VibrationEffect
+import android.os.Vibrator
 import android.util.Log
 import android.widget.TextView
 
@@ -8,14 +11,21 @@ class SetPomodoroTimer(
     private var studyTime: Int,
     private var breakTime: Int,
     private var studyTextView: TextView,
-    private var breakTextView: TextView
+    private var breakTextView: TextView,
+    private var vibrator: Vibrator
 ) {
     private var studyTimer: CountDownTimer? = null
     private var breakTimer: CountDownTimer? = null
+    private var minutes: Long = 0L
+    private var seconds: Long = 0L
     private var remainingStudyTime: Long = (studyTime * 60 * 1000).toLong()
     private var remainingBreakTime: Long = (breakTime * 60 * 1000).toLong()
     private var isStudyTime = true
-    private var countStudyCycle: Int = 0;
+    private var cycle: Int = 0
+
+
+    private var totalStudyTime: Int = 0
+
 
     fun startTimer() {
         if (isStudyTime) {
@@ -26,28 +36,27 @@ class SetPomodoroTimer(
     }
 
     private fun startStudyTimer() {
-        Log.d("startStudyTimer","startStudyTimer")
-        ++countStudyCycle
 
         studyTimer = object : CountDownTimer(remainingStudyTime, 1000) {
             override fun onTick(millisUntilFinished: Long) {
                 remainingStudyTime = millisUntilFinished
-                val minutes = millisUntilFinished / 60000
-                val seconds = (millisUntilFinished % 60000) / 1000
+                minutes = millisUntilFinished / 60000
+                seconds = (millisUntilFinished % 60000) / 1000
                 studyTextView.text = String.format("%02d:%02d", minutes, seconds)
             }
 
             override fun onFinish() {
+                ++cycle
                 studyTextView.text = "${studyTime}:00"
                 isStudyTime = false
                 remainingBreakTime = (breakTime * 60 * 1000).toLong()
+                executionViberate()
                 startBreakTimer()
             }
         }.start()
     }
 
     private fun startBreakTimer() {
-        Log.d("startBreakTimer()","startBreakTimer()")
         breakTimer = object : CountDownTimer(remainingBreakTime, 1000) {
             override fun onTick(millisUntilFinished: Long) {
                 remainingBreakTime = millisUntilFinished
@@ -60,26 +69,27 @@ class SetPomodoroTimer(
                 breakTextView.text = "${breakTime}:00"
                 isStudyTime = true
                 remainingStudyTime = (studyTime * 60 * 1000).toLong()
+                executionViberate()
                 startStudyTimer()
             }
         }.start()
     }
 
     fun pauseTimer() {
-        Log.d("pauseTimer()","pauseTimer()")
         studyTimer?.cancel()
         breakTimer?.cancel()
     }
 
     fun resumeTimer() {
-        Log.d("resumeTimer()","resumeTimer()")
-        Log.i("isStudyTime value : ","${isStudyTime}")
         startTimer()
     }
 
-    fun resetTimer(){
-        Log.d("resetTimer()","resetTimer()")
-        Log.i("isStudyTime value : ","${isStudyTime}")
+    fun resetTimer() {
+        totalStudyTime = cycle * studyTime + (studyTime - minutes.toInt())
+        Log.i(
+            "checkValues", "cycle : ${cycle}, \n " +
+                    "studyTime : ${studyTime}, minutes : ${minutes}, totalStudyTime : ${totalStudyTime}"
+        )
         studyTimer?.cancel()
         breakTimer?.cancel()
         remainingStudyTime = 0L
@@ -87,14 +97,27 @@ class SetPomodoroTimer(
         isStudyTime = false
         breakTextView.text = "${breakTime}:00"
         studyTextView.text = "${studyTime}:00"
-        Log.d("resetTimer()","resetTimer()")
-        Log.i("isStudyTime value : ","${isStudyTime}")
+        cycle = 0
 
     }
 
-    fun changeStudyTimeAndBreakTimeForCustomMode(){
+    fun customModeTime() {
         studyTime = 0
         breakTime = 0
+    }
+
+
+    fun executionViberate() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            vibrator.vibrate(
+                VibrationEffect.createOneShot(
+                    500,
+                    VibrationEffect.DEFAULT_AMPLITUDE
+                )
+            )
+        } else {
+            vibrator.vibrate(500)
+        }
     }
 }
 
